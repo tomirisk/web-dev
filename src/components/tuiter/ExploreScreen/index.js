b3e45dd5bdc2d5a5ac8938df127faeec0a8d61ed
tomiris <kozhabekkyzy.t@northeastern.edu>
2022-03-28 22:03:05 -0400

    A7 second commit

--- src/components/tuiter/ExploreScreen/index.js
+++ src/components/tuiter/ExploreScreen/index.js
@@ -1,15 +1,9 @@
 import React from "react";
-
-import NavigationSidebar from "../NavigationSidebar";
 import ExploreComponent from "./ExploreComponent";
-import WhoToFollowList from "../WhoToFollowList";
 
 const ExploreScreen = () => {
   return(
-
         <ExploreComponent/>
-
-
   );
 };
 export default ExploreScreen;
