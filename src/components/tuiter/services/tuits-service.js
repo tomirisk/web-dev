a41763be29551bbf2b0cc0222646966672d5e024
tomiris <kozhabekkyzy.t@northeastern.edu>
2022-04-06 18:14:06 -0400

    Fixed API base

--- src/components/tuiter/services/tuits-service.js
+++ src/components/tuiter/services/tuits-service.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 //const TUITS_API = 'http://localhost:4000/api/tuits';
 //const TUITS_API = 'https://web-dev-node-project.herokuapp.com/api/tuits';
-const TUITS_API = `${API_BASE}/tuits`;
 const API_BASE = process.env.REACT_APP_API_BASE;
+const TUITS_API = `${API_BASE}/tuits`;
 
 export const createTuit = async (tuit) => {
   const response = await axios.post(TUITS_API, tuit)
