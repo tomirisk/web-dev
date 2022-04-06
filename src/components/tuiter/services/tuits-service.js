8987522c03119be906ae52ab6089f10b8f4f0a70
tomiris <kozhabekkyzy.t@northeastern.edu>
2022-04-06 18:54:19 -0400

    test commit

--- src/components/tuiter/services/tuits-service.js
+++ src/components/tuiter/services/tuits-service.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 //const TUITS_API = 'https://web-dev-node-project.herokuapp.com/api/tuits';
 const API_BASE = process.env.REACT_APP_API_BASE;
 const TUITS_API = `${API_BASE}/tuits`;
+alert(API_BASE);
 
 export const createTuit = async (tuit) => {
   const response = await axios.post(TUITS_API, tuit)
